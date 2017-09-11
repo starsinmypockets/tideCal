ab1bbc8d0c25081a895a99fcd3c01b8859deb575
paul walker <paulwalker@pauls-MacBook-Pro.local>
2017-09-11 18:14:20 -0400

    The rudiments of a form

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,8 @@
 port module Main exposing (..)
 
-import Html exposing (Html, text, div, img, h1, h2, h3, p, button, ul, li)
-import Html.Attributes exposing (src, class, classList)
-import Html.Events exposing (onClick)
+import Html exposing (Html, text, div, img, h1, h2, h3, p, button, ul, li, input, label, form, fieldset)
+import Html.Attributes exposing (src, class, classList, id, for, type_, defaultValue, name, value)
+import Html.Events exposing (onClick, onInput, onWithOptions, Options)
 import Debug exposing (log)
 import Json.Decode exposing (..)
 import Tuple
@@ -17,11 +17,15 @@ type alias Cal =
 
 type alias Model =
     { client_id : String
-    , count : Int
     , discovery_docs : List String
     , scopes : String
     , messages : List ApiRes
     , calendars : List Cal
+    , startDate : String
+    , endDate : String
+    , station : String
+    , calName : String
+    , units : String
     }
 
 
@@ -30,9 +34,13 @@ model =
     { client_id = "787419036517-pqu3ga58d833sr5c81jgebkdre0q9t76.apps.googleusercontent.com"
     , discovery_docs = [ "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest" ]
     , scopes = "https://www.googleapis.com/auth/calendar"
-    , count = 0
     , messages = []
     , calendars = []
+    , startDate = ""
+    , endDate = ""
+    , station = ""
+    , calName = ""
+    , units = "English"
     }
 
 
@@ -66,16 +74,22 @@ type Msg
     | Send ( List String, String )
     | Rcv ( GapiRes, ApiCmd )
     | NoOp
+    | Submit
+    | Station String
+    | StartDate String
+    | EndDate String
+    | CalName String
+    | Units String
 
 
 doAuth : Msg -> Model -> ( Model, Cmd Msg )
 doAuth msg model =
-    ( { model | count = model.count + 10 }, Cmd.none )
+    ( model, Cmd.none )
 
 
 doSignout : Msg -> Model -> ( Model, Cmd Msg )
 doSignout msg model =
-    ( { model | count = 0 }, Cmd.none )
+    ( model, Cmd.none )
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -92,6 +106,29 @@ update msg model =
 
         Rcv res ->
             handleApiRes res model
+
+        Submit ->
+            let
+                m =
+                    model
+                        |> log "MODEL"
+            in
+                ( model, Cmd.none )
+
+        Station txt ->
+            ( { model | station = txt }, Cmd.none )
+
+        StartDate date ->
+            ( { model | startDate = date }, Cmd.none )
+
+        EndDate date ->
+            ( { model | endDate = date }, Cmd.none )
+
+        CalName name ->
+            ( { model | calName = name }, Cmd.none )
+
+        Units txt ->
+            ( { model | units = txt }, Cmd.none )
 
         _ ->
             ( model, Cmd.none )
@@ -149,13 +186,13 @@ handleApiRes res model_ =
 
 introText : String
 introText =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+    "Uses the https://tidesandcurrents.noaa.gov/api/ API to generate google calendar events and to import them into a new or existing calendar. ** DISCLAIMER ** This is a beta product - please use at your own discretion. To contribute or report an issue visit \n    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
 
 
 view : Model -> Html Msg
 view model =
     div [ classList [ ( "row", True ), ( "container", True ) ] ]
-        [ h1 [] [ text " Tides for Google Calendar " ]
+        [ h1 [] [ text "Tides for Google Calendar" ]
         , p [] [ text introText ]
         , div [ class "row" ]
             [ h2 [] [ text "Your calendars" ]
@@ -167,6 +204,29 @@ view model =
             , button [ onClick (Send ( [], "init" )) ] [ text "INIT" ]
             ]
         , div [ class "row" ]
+            [ h2 [ for "station_id" ] [ text "Select Tide Information" ]
+            , form [ class "row" ]
+                [ label [ for "station_id", class "col-md-4" ] [ text "Station Id" ]
+                , input [ id "station_id", class "col-md-4", onInput Station ] []
+                , label [ for "start", class "col-md-4" ] [ text "Start Date" ]
+                , input [ id "start", class "col-md-4", onInput StartDate ] []
+                , label [ for "end", class "col-md-4" ] [ text "End Date" ]
+                , input [ id "end", class "col-md-4", onInput EndDate ] []
+                , label [ for "calendar_name", class "col-md-4" ] [ text "Enter calendar name for import" ]
+                , input [ id "calendar_name ", class "col-md-4", onInput CalName ] []
+                , fieldset [ class "row", onInput Units ]
+                    [ p [ class "col-md-3" ] [ text "Units:" ]
+                    , radio "English"
+                    , radio "Metric"
+                    , button [ onWithOptions "click" { preventDefault = True, stopPropagation = True } (Json.Decode.succeed Submit) ] [ text "Submit" ]
+                    ]
+
+                -- radio english / metric
+                -- time zone default your time zone
+                --
+                ]
+            ]
+        , div [ class "row" ]
             [ h2 [] [ text "debug log" ]
             , ul [] (messageList model.messages)
             ]
@@ -175,6 +235,15 @@ view model =
 
 
 -- template bits
+
+
+radio : String -> Html msg
+radio txt =
+    label
+        [ class "radios" ]
+        [ input [ type_ "radio", name "units" ] []
+        , text txt
+        ]
 
 
 messageList : List ApiRes -> List (Html msg)
